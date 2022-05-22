3cfdf9480874aa45809eefb5d6fca64bce8eb257
Nwele Uchenna <nweleuchenna2004@gmail.com>
2022-05-22 13:14:10 +0100

    finished assignment

---
+++ components/header.js
@@ -0,0 +1,16 @@
+function Header() {
+    return (
+        <div className="header">
+            <div>
+                Web3Workout
+            </div>
+
+            <ul>
+                <li>Iteration</li>
+                <li>Leadership</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Header
---
+++ pages/leadership.js
@@ -0,0 +1,19 @@
+import Header from "../components/header";
+
+function Leadership() {
+    return (
+        <div>
+            {/* this page would display the top ten workout participant */}
+            <Header />
+            <br />
+
+            <ul>
+                <li>
+                    <p>Name</p> <p>Iteration</p> <p>Steps</p>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Leadership
